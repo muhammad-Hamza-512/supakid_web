ccb86d6e66b3ae173744e8d83e72442882db0ae3
Muhammad Hamza <mhamzau3291@gmail.com>
2025-12-09 17:56:11 +0500

    Add build web folder

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -131,7 +131,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a931364aa89278e97ec31163e10b6b76
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "75aad1c46fe1068e96be7f944beb4672",
+"flutter_bootstrap.js": "f54c8393269906f0c008bc135729cfe7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
