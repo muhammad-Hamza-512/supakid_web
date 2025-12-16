b9b4295b6d988d2cf4878575740153056b1eae5d
Muhammad Hamza <mhamzau3291@gmail.com>
2025-12-16 21:33:25 +0500

    latest

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -131,7 +131,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a931364aa89278e97ec31163e10b6b76
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "c89243239df15016f58b01994e9b7e2d",
+"flutter_bootstrap.js": "b2ebc545bea1ac951616516ac5a1d45b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
