357c6622ff119dec711e6e12e5a10a947774f7c8
Muhammad Hamza <mhamzau3291@gmail.com>
2025-12-17 00:08:40 +0500

    latest code

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "220437374"
+    serviceWorkerVersion: "262751316"
   }
 });
